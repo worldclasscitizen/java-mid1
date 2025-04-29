df725fe51056f1a7affee2ee263fba939bf5096e
teddynu <jk62362@mensakorea.org>
2025-04-29 12:19:00 +0900

    feat: 강의 실습 코드 추가

--- src/lang/immutable/address/RefMain1_2.java
+++ src/lang/immutable/address/RefMain1_2.java
@@ -1,17 +1,17 @@
 package lang.immutable.address;
 
-public class RefMain1_1 {
+public class RefMain1_2 {
 
     public static void main(String[] args) {
         // 참조형 변수는 하나의 인스턴스를 공유할 수 있다.
-        Address a = new Address("서울");
-        Address b = a;
+        Address a = new Address("서울"); // x001
+        Address b = new Address("서울"); // x002
         System.out.println("a = " + a);
         System.out.println("b = " + b);
 
-        b.setValue("부산"); // b의 값을 부산으로 변경해야 함
+        b.setValue("부산");
         System.out.println("부산 -> b");
-        System.out.println("a = " + a); // 사이드 이펙트 발생
+        System.out.println("a = " + a);
         System.out.println("b = " + b);
     }
 }
--- src/lang/immutable/address/RefMain1_3.java
+++ src/lang/immutable/address/RefMain1_3.java
@@ -1,6 +1,6 @@
 package lang.immutable.address;
 
-public class RefMain1_1 {
+public class RefMain1_3 {
 
     public static void main(String[] args) {
         // 참조형 변수는 하나의 인스턴스를 공유할 수 있다.
@@ -9,9 +9,13 @@ public class RefMain1_1 {
         System.out.println("a = " + a);
         System.out.println("b = " + b);
 
-        b.setValue("부산"); // b의 값을 부산으로 변경해야 함
-        System.out.println("부산 -> b");
-        System.out.println("a = " + a); // 사이드 이펙트 발생
+        change(b, "부산");
+        System.out.println("a = " + a);
         System.out.println("b = " + b);
+    }
+
+    private static void change(Address address, String changeAddress) {
+        System.out.println("주소 값을 변경합니다 -> " + changeAddress);
+        address.setValue(changeAddress);
     }
 }
