8344bb427987cc961ddbbdd1e902d3f0096860c8
teddynu <jk62362@mensakorea.org>
2025-05-30 09:00:07 +0900

    feat: 강의 실습 코드 추가

---
+++ src/lang/Math/RandomMain.java
@@ -0,0 +1,27 @@
+package lang.Math;
+
+import java.util.Random;
+
+public class RandomMain {
+
+    public static void main(String[] args) {
+        Random random = new Random();
+//        Random random = new Random(1); // seed 가 같으면 Random 의 결과가 같다.
+
+        int randomInt = random.nextInt();
+        System.out.println("randomInt = " + randomInt);
+
+        double randomDouble = random.nextDouble(); // 0.0d ~ 1.0d
+        System.out.println("randomDouble = " + randomDouble);
+
+        boolean randomBoolean = random.nextBoolean();
+        System.out.println("randomBoolean = " + randomBoolean);
+
+        // 범위 조회
+        int randomRange1 = random.nextInt(10);// 0 ~ 9까지 출력
+        System.out.println("0 ~ 9 = " + randomRange1);
+
+        int randomRange2 = random.nextInt(10) + 1;
+        System.out.println("1 ~ 10 = " + randomRange2);
+    }
+}
