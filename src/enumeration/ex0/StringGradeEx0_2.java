48aa4406b8b3b0b1481a5c9a40a74cac8414c246
teddynu <jk62362@mensakorea.org>
2025-06-01 19:09:07 +0900

    feat: 강의 실습 코드 추가

---
+++ src/enumeration/ex0/StringGradeEx0_2.java
@@ -0,0 +1,30 @@
+package enumeration.ex0;
+
+public class StringGradeEx0_2 {
+
+    public static void main(String[] args) {
+        int price = 10000;
+
+        DiscountService discountService = new DiscountService();
+
+        // 존재하지 않는 등급
+        int vip = discountService.discount("VIP", price);
+        System.out.println("VIP 등급의 할인 금액 : " + vip);
+
+        // 오타
+        int diamondd = discountService.discount("DIAMONDD", price);
+        System.out.println("DIAMONDD 등급의 할인 금액 : " + diamondd);
+
+        // 소문자
+        int gold1 = discountService.discount("gold", price);
+        System.out.println("gold 등급의 할인 금액 : " + gold1);
+
+        int basic = discountService.discount("BASIC", price);
+        int gold = discountService.discount("GOLD", price);
+        int diamond = discountService.discount("DIAMOND", price);
+
+        System.out.println("BASIC 등급의 할인 금액 : " + basic);
+        System.out.println("GOLD 등급의 할인 금액 : " + gold);
+        System.out.println("DIAMOND 등급의 할인 금액 : " + diamond);
+    }
+}
